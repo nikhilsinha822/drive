745fb86394c93bdcf3e281557b48945ff76816ee
nikhilsinha822 <nikhilsinha822@gmail.com>
2024-07-12 02:00:19 +0530

    route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/home"
+import Login from "./pages/(auth)/login"
+import Register from "./pages/(auth)/register"
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path="/" index element={<Home />} />
+      <Route path="/login" index element={<Login />} />
+      <Route path="/register" index element={<Register />} />
+    </Routes>
   )
 }
 
